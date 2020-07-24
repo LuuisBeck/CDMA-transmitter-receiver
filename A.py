7d9ae0fb8d6da3a8d7eacaf749a69f128cebdc42
Luis Becerra Uribe <lb.becerra.1995@gmail.com>
2020-07-23 22:37:56 -0400

    added sounddevice to reproduce sound

--- A.py
+++ A.py
@@ -1,6 +1,7 @@
 import matplotlib.pylab as pyl
 from transform import imageBW_to_bits, bits_to_imageBW
 from cdma import from_bits_to_CDMA
+import sounddevice as sd
 
 def customModulation(ts, fc, bits):
     # this custom ASK modulation (amplitude-shift modulation) is for support
@@ -14,9 +15,9 @@ def customModulation(ts, fc, bits):
             if (bits[i] == 0):
                 A_for_Bit_i = 0
             elif (bits[i] == 2):
-                A_for_Bit_i = 4
+                A_for_Bit_i = 5
             elif (bits[i] == -2):
-                A_for_Bit_i = 2
+                A_for_Bit_i = 1
             A.append(A_for_Bit_i)
     return A * pyl.sin(2.0 * pyl.pi * fc * ts)
 
@@ -32,6 +33,7 @@ bits2 = imageBW_to_bits(file2)
 
 #-----bits to CDMA-----#
 cdma = from_bits_to_CDMA(bits1, bits2)
+print(cdma)
 
 #-----Transmit CDMA via AM and OOK modulation-----#
 # time between samples
@@ -45,6 +47,10 @@ fc = 800
 
 y = customModulation(ts, fc, cdma)
 # Plotting results
+
+fs = 44100
+sd.play(y, fs, blocking=True)
+
 pyl.plot(ts, y)
 pyl.xlabel("tiempo (s)")
 pyl.ylabel("Nivel de senal")
